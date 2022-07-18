9a969b6791ce9d072e88fdbb9d5cb18e999c8a14
raulriato <raulriato@gmail.com>
2022-07-18 16:50:29 -0300

    feat like button change

--- src/PostBottom.js
+++ src/PostBottom.js
@@ -1,9 +1,22 @@
+import React from "react";
+
 export default function PostBottom(props){
+    const [heart, setHeart] = React.useState("heart-outline");
+
+    function handleLike(){
+        console.log(heart);
+        if(heart === 'heart-outline'){
+            setHeart('heart');
+        } else {
+            setHeart('heart-outline');
+        }
+        console.log(heart);
+    }
     return (
         <div class="post-bottom">
             <div class="icons">
                 <div class="icons-left">
-                    <ion-icon class="like" name="heart-outline"></ion-icon>
+                    <ion-icon name={heart} onClick={handleLike}></ion-icon>
                     <ion-icon class="comment" name="chatbubble-outline"></ion-icon>
                     <ion-icon class="dm" name="paper-plane-outline"></ion-icon>
                 </div>
@@ -13,7 +26,6 @@ export default function PostBottom(props){
                 <img src={props.img} class="liked-profile" alt="" />
                 <h4>Curtido por <strong>{props.profile}</strong> e <strong>outras 453.907 pessoas</strong> </h4>
             </div>
-            
         </div>
     )
 }
